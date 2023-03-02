70a213967501c03d1a1b8626215e3f32dc38264f
Максим <zimovets3@yandex.ru>
2023-03-02 21:27:33 +0300

    Homework 9

---
+++ TS1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите значение M: ");
+int n = int.Parse(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int m = int.Parse(Console.ReadLine());
+for (int i = n; i <= m; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i + " ");
+    }
+}
---
+++ TS2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите значение M: ");
+int numM = int.Parse(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int numN = int.Parse(Console.ReadLine());
+
+void Numbers (int numM, int numN, int NumSum)
+{
+    if (numM > numN) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {NumSum}"); 
+        return;
+    }
+    NumSum = NumSum + (numM++);
+    Numbers(numM, numN, NumSum);
+}
+
+Numbers(numM, numN, 0);
